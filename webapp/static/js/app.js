6cc1b7a5153c058d8f3013759c8a1d39c5899d78
Andrew Archbold <andrew.archbold@quiet.ly>
2017-02-27 16:24:54 -0800

    making logo more crisp on retina devices

--- webapp/static/js/app.js
+++ webapp/static/js/app.js
@@ -1,1 +1,94 @@
-$.fn.handleNewsletter=function(){function e(){i.prop("disabled",!0),o.prop("disabled",!0),u.show()}function s(){i.prop("disabled",!1),o.prop("disabled",!1),u.hide()}function n(){i.removeClass("-error"),l.hide(),a.hide(),i.val(i.val().trim()),m.test(i.val())?(e(),t(i.val())):(d.html(c),l.show(),i.addClass("-error"))}function t(e){console.log("signs up for email service");var n="https://cxmdzic2yc.execute-api.us-east-1.amazonaws.com/prod/track",t="email="+e;$.post(n,t,function(){a.show(),l.hide(),p.hide(),s()}).fail(function(e){e&&e.responseText?d.html(JSON.parse(e.responseText).error):d.html(c),a.hide(),l.show(),i.addClass("-error"),s()})}var r=$(this),i=$(".newsletter-signup__input",r),o=$(".button__signup",r),a=$(".newsletter-signup__form.-success",r),l=$(".newsletter-signup__form.-fail",r),p=$(".newsletter-signup__form.-entry",r),u=$(".newsletter-signup__spinner",r),d=$(".newsletter-signup__error-msg",r),c="Oops! Something went wrong. Please make sure you've entered a valid Email address.",m=/^[-a-z0-9~!$%^&*_=+}{\'?]+(\.[-a-z0-9~!$%^&*_=+}{\'?]+)*@([a-z0-9_][-a-z0-9_]*(\.[-a-z0-9_]+)*\.(aero|arpa|biz|com|coop|edu|gov|info|int|mil|museum|name|net|org|pro|travel|mobi|[a-z][a-z])|([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}))(:[0-9]{1,5})?$/i;o.click(function(e){e.preventDefault(),n()}),i.keyup(function(e){13===e.which&&n()})},$(function(){$(".newsletter-signup").handleNewsletter()});
+/* eslint-disable */
+
+// Newsletter Signup Handling
+$.fn.handleNewsletter = function(){
+  var context = $(this),
+    $input = $('.newsletter-signup__input', context),
+    $button = $('.button__signup', context),
+    $success = $('.newsletter-signup__form.-success', context),
+    $fail = $('.newsletter-signup__form.-fail', context),
+    $entry = $('.newsletter-signup__form.-entry', context),
+    $spinner = $('.newsletter-signup__spinner', context),
+    $errorMsg = $('.newsletter-signup__error-msg', context),
+    errorDefault = 'Oops! Something went wrong. Please make sure you\'ve entered a valid Email address.',
+    validEmail = /^[-a-z0-9~!$%^&*_=+}{\'?]+(\.[-a-z0-9~!$%^&*_=+}{\'?]+)*@([a-z0-9_][-a-z0-9_]*(\.[-a-z0-9_]+)*\.(aero|arpa|biz|com|coop|edu|gov|info|int|mil|museum|name|net|org|pro|travel|mobi|[a-z][a-z])|([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}))(:[0-9]{1,5})?$/i;
+
+
+  function disableForm() {
+    $input.prop('disabled', true);
+    $button.prop('disabled', true);
+    $spinner.show();
+  }
+
+  function enableForm() {
+    $input.prop('disabled', false);
+    $button.prop('disabled', false);
+    $spinner.hide();
+  }
+
+  function submitForm() {
+    $input.removeClass('-error');
+    $fail.hide();
+    $success.hide();
+    // make sure the string doesn't have leading or trailing whitespace
+    $input.val($input.val().trim());
+
+    // make sure email is valid.
+    if (validEmail.test($input.val())) {
+      // send to service
+      disableForm();
+      newsletterSignup($input.val());
+    } else {
+      $errorMsg.html(errorDefault);
+      $fail.show();
+      $input.addClass('-error');
+    };
+  }
+
+  function newsletterSignup(value) {
+    console.log('signs up for email service');
+    var signupUrl = 'https://cxmdzic2yc.execute-api.us-east-1.amazonaws.com/prod/track';
+    var postData = 'email=' + value;
+
+    // TODO: wire this up with proper 
+    $.post(signupUrl, postData, function() {
+      // successful sign up
+      $success.show();
+      $fail.hide();
+      $entry.hide();
+      enableForm();
+    })
+    .fail(function(response) {
+      // failed signup
+      if (response && response.responseText) {
+        // insert error message from server
+        $errorMsg.html(JSON.parse(response.responseText).error);
+      } else {
+        // show default error
+        $errorMsg.html(errorDefault);
+      }
+      $success.hide();
+      $fail.show();
+      $input.addClass('-error');
+      enableForm();
+    });
+  }
+
+  $button.click(function(e) {
+    e.preventDefault();
+    submitForm();
+  });
+
+  $input.keyup(function(e) {
+    if (e.which === 13) {
+      submitForm();
+    }
+  });
+
+}
+
+
+$(function(){
+  $('.newsletter-signup').handleNewsletter();
+});
+
