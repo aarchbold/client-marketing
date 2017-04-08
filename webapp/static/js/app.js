613f1fa95673c2eaf28024284faddb5152dd72ed
Andrew Archbold <andrew.archbold@quiet.ly>
2017-04-08 12:49:25 -0700

    #time 1h 0m adding featured in section and started the features section

--- webapp/static/js/app.js
+++ webapp/static/js/app.js
@@ -1,1 +1,23 @@
-$(function(){window.setTimeout(function(){$(".intro-shiffy").addClass("-show-shiffy").delay(500).queue(function(i){$(".intro-shiffy__quote").addClass("-animate"),$(),i()})},1e3),window.setTimeout(function(){$(".intro-shiffy__quote-text").show()},1800)});
+/* eslint-disable */
+
+
+
+$(function(){
+  // animate shiffy
+  window.setTimeout(function(){
+    $('.intro-shiffy').addClass('-show-shiffy').delay(500).queue(function(next) {
+      $('.intro-shiffy__quote').addClass('-animate');
+      $();
+      next();
+    });
+  }, 1000)
+  window.setTimeout(function(){
+    $('.intro-shiffy__quote-text').show();
+  }, 1800)
+  
+});
+
+// $("#div").addClass("error").delay(1000).queue(function(next){
+//     $(this).removeClass("error");
+//     next();
+// });
