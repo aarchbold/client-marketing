87a0142838497ea5b5de588821d23632cb6ef57f
dc85 <daniel85.chen@gmail.com>
2017-04-19 15:54:35 -0700

    http://

--- webapp/deploy.sh
+++ webapp/deploy.sh
@@ -1,2 +1,3 @@
 aws s3 cp index.html s3://www.myshyft.com --profile default
 aws s3 sync static s3://www.myshyft.com/static --profile default
+aws s3 sync business s3://www.myshyft.com/business --profile default
