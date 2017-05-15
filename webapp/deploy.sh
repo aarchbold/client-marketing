6615327824aa24cbef4b88eda577c8fe0fd03fb4
dc85 <daniel85.chen@gmail.com>
2017-05-15 10:49:51 -0700

    added pages to deploy' '

--- webapp/deploy.sh
+++ webapp/deploy.sh
@@ -1,3 +1,5 @@
 aws s3 cp index.html s3://www.myshyft.com --profile default
+aws s3 cp privacy.html s3://www.myshyft.com --profile default
+aws s3 cp terms.html s3://www.myshyft.com --profile default
 aws s3 sync static s3://www.myshyft.com/static --profile default
 aws s3 sync business s3://www.myshyft.com/business --profile default
