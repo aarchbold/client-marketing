6011f063228a6dd12b89399f57eb40d8386ba6bb
dc85 <daniel85.chen@gmail.com>
2017-06-27 19:47:03 -0700

    updated privacy for old navy

--- webapp/deploy.sh
+++ webapp/deploy.sh
@@ -2,5 +2,6 @@ aws s3 cp index.html s3://www.myshyft.com --profile default
 aws s3 cp privacy.html s3://www.myshyft.com --profile default
 aws s3 cp terms.html s3://www.myshyft.com --profile default
 aws s3 cp terms-app.html s3://www.myshyft.com --profile default
+aws s3 cp old-navy-privacy.html s3://www.myshyft.com --profile default
 aws s3 sync static s3://www.myshyft.com/static --profile default
 aws s3 sync business s3://www.myshyft.com/business --profile default
