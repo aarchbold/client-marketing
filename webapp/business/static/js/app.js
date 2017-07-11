5ef3b9b97f0bdb70026475df20853c435209a616
Andrew Archbold <andrew.archbold@quiet.ly>
2017-07-10 23:33:53 -0700

    #time 2h 0m business home page dev

--- webapp/business/static/js/app.js
+++ webapp/business/static/js/app.js
@@ -1,6 +1,25 @@
 /* eslint-disable */
 
+$.fn.handleMenu = function() {
+    var context = $(this),
+        $menuBtn = $('.main-navigation__mobile-button', context),
+        $menu = $('.main-navigation__business', context);
+
+    $menuBtn.click(function(e) {
+        e.preventDefault();
+        if ($menuBtn.hasClass('fa-bars')) {
+            $menuBtn.addClass('fa-times');
+            $menuBtn.removeClass('fa-bars');
+            $menu.fadeIn();
+        } else {
+            $menuBtn.addClass('fa-bars');
+            $menuBtn.removeClass('fa-times');
+            $menu.fadeOut();
+        };
+    });
+};
+
 $(function(){
   // alert('hi');
+  $('.main-navigation').handleMenu()
 });
-
