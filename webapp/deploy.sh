7e383af02a573af8f3e0de883b9d58bf8db33040
dc85 <daniel85.chen@gmail.com>
2018-01-22 17:30:05 -0800

    typo

--- webapp/deploy.sh
+++ webapp/deploy.sh
@@ -7,8 +7,10 @@ aws s3 cp terms-app.html s3://www.myshyft.com --profile default
 aws s3 cp old-navy-privacy.html s3://www.myshyft.com --profile default
 aws s3 cp how.html s3://www.myshyft.com --profile default
 aws s3 cp how-features.html s3://www.myshyft.com --profile default
+aws s3 cp features.html s3://www.myshyft.com --profile default
 aws s3 cp security.html s3://www.myshyft.com --profile default
 aws s3 cp compliance.html s3://www.myshyft.com --profile default
+aws s3 cp workforce.html s3://www.myshyft.com --profile default
 aws s3 cp business.html s3://www.myshyft.com --profile default
 aws s3 sync static s3://www.myshyft.com/static --profile default
 aws s3 sync business s3://www.myshyft.com/business --profile default
