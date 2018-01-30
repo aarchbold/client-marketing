59c669944d92a0a450b3b16aabd40ee7cd31a596
Andrew Archbold <andrew.archbold@quiet.ly>
2018-01-29 22:43:55 -0800

    #time 1h 0m adding animations to the features phone images

--- webapp/business/static/js/app.js
+++ webapp/business/static/js/app.js
@@ -1,5 +1,37 @@
 /* eslint-disable */
 
+$.fn.handleFeaturesAnimations = function() {
+    console.log('hello?');
+    console.log($(this));
+
+    var $animation_elements = $('.features__phones-inner',$(this));
+    var $window = $(window);
+
+    function check_if_in_view() {
+      var window_height = $window.height();
+      var window_top_position = $window.scrollTop();
+      var window_bottom_position = (window_top_position + window_height);
+
+      $.each($animation_elements, function() {
+        var $element = $(this);
+        var element_height = $element.outerHeight();
+        var element_top_position = $element.offset().top;
+        var element_bottom_position = (element_top_position + element_height);
+
+        //check to see if this current container is within viewport
+        if ((element_bottom_position >= window_top_position) &&
+          (element_top_position <= window_bottom_position)) {
+          $element.addClass('in-view');
+        } else {
+          $element.removeClass('in-view');
+        }
+      });
+    }
+
+    $window.on('scroll resize', check_if_in_view);
+    $window.trigger('scroll');
+}
+
 $.fn.makeEqualHeight = function() {
     var context = $(this),
         $tiles = $('.home-featurs__item.-equal', context),
@@ -424,5 +456,7 @@ $(function(){
   });
 
   $('.home-features').makeEqualHeight();
+
+  $('.section-features__inner').handleFeaturesAnimations()
 
 });
