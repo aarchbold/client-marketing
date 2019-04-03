33ccf8db2e3f825891c7ac464380ad3f56c2def7
Taylor Case <taylor@myshyft.com>
2019-04-03 10:55:36 -0700

    add google careers page instead of smartrecruiters

--- webapp/deploy.sh
+++ webapp/deploy.sh
@@ -1,17 +1,17 @@
 aws s3 cp index.html s3://www.myshyft.com --profile default
-aws s3 cp google1a72dfd52304756f.html s3://www.myshyft.com --profile default
-aws s3 cp survey.html s3://www.myshyft.com --profile default
-aws s3 cp privacy.html s3://www.myshyft.com --profile default
-aws s3 cp terms.html s3://www.myshyft.com --profile default
-aws s3 cp terms-app.html s3://www.myshyft.com --profile default
-aws s3 cp old-navy-privacy.html s3://www.myshyft.com --profile default
-aws s3 cp gap-privacy.html s3://www.myshyft.com --profile default
-aws s3 cp how.html s3://www.myshyft.com --profile default
-aws s3 cp how-features.html s3://www.myshyft.com --profile default
-aws s3 cp features.html s3://www.myshyft.com --profile default
-aws s3 cp security.html s3://www.myshyft.com --profile default
-aws s3 cp compliance.html s3://www.myshyft.com --profile default
-aws s3 cp workforce.html s3://www.myshyft.com --profile default
-aws s3 cp business.html s3://www.myshyft.com --profile default
-aws s3 sync static s3://www.myshyft.com/static --profile default
-aws s3 sync business s3://www.myshyft.com/business --profile default
+# aws s3 cp google1a72dfd52304756f.html s3://www.myshyft.com --profile default
+# aws s3 cp survey.html s3://www.myshyft.com --profile default
+# aws s3 cp privacy.html s3://www.myshyft.com --profile default
+# aws s3 cp terms.html s3://www.myshyft.com --profile default
+# aws s3 cp terms-app.html s3://www.myshyft.com --profile default
+# aws s3 cp old-navy-privacy.html s3://www.myshyft.com --profile default
+# aws s3 cp gap-privacy.html s3://www.myshyft.com --profile default
+# aws s3 cp how.html s3://www.myshyft.com --profile default
+# aws s3 cp how-features.html s3://www.myshyft.com --profile default
+# aws s3 cp features.html s3://www.myshyft.com --profile default
+# aws s3 cp security.html s3://www.myshyft.com --profile default
+# aws s3 cp compliance.html s3://www.myshyft.com --profile default
+# aws s3 cp workforce.html s3://www.myshyft.com --profile default
+# aws s3 cp business.html s3://www.myshyft.com --profile default
+# aws s3 cp static s3://www.myshyft.com/static --profile default --recursive
+# aws s3 cp business s3://www.myshyft.com/business --profile default --recursive
